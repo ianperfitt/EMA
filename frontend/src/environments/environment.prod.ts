ddec002ab07fcb1758826a608c6e8589471d8d3d
Ian Perfitt <iancp@protonnmail.com>
2024-06-19 11:52:54 -0500

    updated npm and node versions

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-// export const environment = {
-// 	production: true,
-// 	apiURL: "http://localhost:8080/api/v1/employees"
-// };
+ //export const environment = {
+ 	//production: true,
+ 	//apiURL: "http://localhost:8080/api/v1/employees"
+ //};
 
 export const environment = {
 	production: true,
