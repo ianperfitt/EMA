19df0f1bf729d59b9092788d2642b790d2a05b2d
Ian Perfitt <141587134+ianperfitt@users.noreply.github.com>
2024-06-19 10:38:39 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+// 	production: true,
+// 	apiURL: "http://localhost:8080/api/v1/employees"
+// };
+
 export const environment = {
 	production: true,
-	apiURL: "http://localhost:8080/api/v1/employees"
+	apiURL: "https://employee-management-app-9d39293ed4dc.herokuapp.com/api/v1/employees"
 };
